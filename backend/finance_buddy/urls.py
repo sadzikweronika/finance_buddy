bbacaee456a4721b2d4245b5acb399d181198874
sadzikweronika <weronikasadzik13@gmail.com>
2025-01-18 09:00:19 +0100

    Added urls for JWT authentication

--- backend/finance_buddy/urls.py
+++ backend/finance_buddy/urls.py
@@ -15,12 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path, re_path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from .views import example_api, login, finances, dashboard, expense_detail, user_profile, ExpenseListCreateAPIView, ExpenseDetailAPIView
 from finance_buddy import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 
 
 schema_view = get_schema_view(
@@ -49,4 +54,9 @@ urlpatterns = [
     path('api/expenses/<int:pk>/', ExpenseDetailAPIView.as_view(), name='expense-detail'),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('register/', views.RegisterView.as_view(), name='register'),
+    path('api/auth/', include('users.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
 ]
