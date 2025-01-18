81535d112fb52a8f7de87bd0e40169280f4f565b
sadzikweronika <weronikasadzik13@gmail.com>
2025-01-18 08:58:54 +0100

    Added view for JWT authentication

--- backend/finance_buddy/views.py
+++ backend/finance_buddy/views.py
@@ -2,9 +2,11 @@ from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
 from rest_framework import status
 from .models import Expense
-from .serializers import ExpenseSerializer
+from .serializers import ExpenseSerializer, UserSerializer
+
 
 def example_api(request):
     data = {
@@ -85,3 +87,13 @@ class ExpenseDetailAPIView(APIView):
 
         expense.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+class RegisterView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
